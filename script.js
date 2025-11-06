897444feaa3eb7e37f8091bfd70b33217e84b841
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-11-06 16:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if (arr.length <= 1) return 0;
+
+    let heap = [...arr];
+    heap.sort((a, b) => a - b);
+
+    let totalCost = 0;
+
+    while (heap.length > 1) {
+        // Take two smallest
+        let first = heap.shift();
+        let second = heap.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        heap.push(cost);
+        heap.sort((a, b) => a - b);
+    }
+    return totalCost;
 }
 
 module.exports=mincost;
